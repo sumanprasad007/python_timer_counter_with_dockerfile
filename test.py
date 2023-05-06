bd35ad9dad88a857cf945d6b88a3ae6ae1283836
Suman Prasad <sumanprasad9766@gmail.com>
2023-05-06 10:11:25 +0530

    added the test cases now modified with version 1.1

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ def test_environment_variable(capsys):
     os.environ['TIMER_SECONDS'] = '10'
     # Run countdown function with environment variable
     exec(open('countdown_timer.py').read())
+    
     # Check that the timer printed the correct output
     captured = capsys.readouterr()
     assert "stop\n" in captured.out
